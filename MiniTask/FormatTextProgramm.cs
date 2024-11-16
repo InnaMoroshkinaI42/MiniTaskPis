616bfef62ec858faab90358af1b72955bc4cddb6
InnaMoroshkinaI42 <innamoroshkina@yandex.ru>
2024-11-17 03:36:42 +0700

    модульные тесты к коду и функции-заглушки

---
+++ MiniTask/FormatTextProgramm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTask
+{
+    class FormatTextProgramm
+    {
+        public static string FormatText(int K, string line)
+        {
+            return string.Empty;
+        }
+
+        public static bool CanFormat(int K, string line)
+        {
+            return true;
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MiniTask;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string result = FormatTextProgramm.FormatText(10, "Проверка");
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [TestMethod]
+        public void TestCanFormat()
+        {
+            bool result = FormatTextProgramm.CanFormat(10, "Проверка2");
+            Assert.IsTrue(result);
+        }
+    }
+}
